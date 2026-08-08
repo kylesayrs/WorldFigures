c95d7c2fe65946a8426b4aec7bf14e4a8ee80431
Kyle Sayers <kylesayrs@gmail.com>
2026-08-07 20:26:03 -0400

    allow hostable on competitivedrawing
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,11 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/worldfigures',
   plugins: [react(), tailwindcss()],
   server: {
     fs: {
       // allow reading ../topics and ../data (siblings of this app) at dev time
       allow: ['..'],
     },
+    allowedHosts: ['competitivedrawing.com'],
   },
 })
